1daff2e6be8752ceef29625b30f9a0a1807737d0
earlgrey02 <san06036@naver.com>
2025-07-20 19:56:57 +0900

    feat: add vpc configuration

---
+++ modules/vpc/main.tf
@@ -0,0 +1,15 @@
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "6.0.1"
+
+  name               = var.name
+  cidr               = var.cidr
+  azs                = var.azs
+  public_subnets     = [for i in range(var.subnet_count / 2) : cidrsubnet(var.cidr, log(var.subnet_count, 2), i)]
+  private_subnets    = [for i in range(var.subnet_count / 2) : cidrsubnet(var.cidr, log(var.subnet_count, 2), i + (var.subnet_count / 2))]
+  enable_nat_gateway = var.enable_nat_gateway
+  tags = {
+    CreatedBy   = "Terraform"
+    Environment = var.env
+  }
+}
